fd2a63cb5b63baf6d78c148c598f141677c49f0d
Alejalons <ux.mor.dez@gmail.com>
2021-02-11 14:58:21 -0300

    desarrollando la accion de comprimir navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Navbar.scss';
 import {Link} from 'react-router-dom';
 
 function Navbar() {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
     return (
         <>
-            <div className="Navbar-left d-flex col-lg-2 bg-light">
-                <div className="d-flex flex-column align-items-start col-lg-12">
+            <div className= 'Navbar-left d-flex col-lg-2 bg-light'>
+                <div className="container d-flex container-items flex-column align-items-start col-lg-12">
 
-                    <div className="mt-2 w logo text-center">
-                        <img src="./images/LogoEliz.png" alt=""/>
+                    <div className="mt-3 w logo row ">
+                        <div className="col-2 mr-3">
+                            <i className={click ? 'fas fa-compress-alt': 'fas fa-expand-alt'} onClick={handleClick}></i>
+                        </div>
+                        <div className=" text-center">
+                            <img src="./images/LogoEliz.png" alt=""/>
+                        </div>
                     </div>
 
                     <div className="mt-3 w btn-add ">
@@ -73,8 +81,13 @@ function Navbar() {
                         </div>
                     </div>
 
-                    <div className="user-people">
-                        
+                    <div className="user-people  w  mt-auto mb-3">
+                        <div className="people mt-2 mr-3">
+                            <img src="./images/people.jpg" alt=""/>
+                        </div>
+                        <div className="description mt-2 ">
+                            <p className="name text-justify">Caskantr <br/> Psicóloga</p>
+                        </div>
                     </div>
 
                 </div>
